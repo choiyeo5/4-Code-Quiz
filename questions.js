3d956e6c23e1d489841857fb62d8efa0565c47cd
Yeon Soo Choi <christinechoi35@gmail.com>
2020-06-18 21:14:40 -0400

    Initiate questions.js, but could not use the variable in script.js so I combined the two

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    
+    // I could not link the two js files together, so I just put the questions variable in script.js
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }
+  ];
--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+var questions = [
+    // I could not fetch data from questions.js
+    // Please do inform me how I can do that. I have tried many things. No success.
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }
+  ];
 
 // Time Given to Solve Problems
 var secondsLeft = 5;
@@ -13,15 +30,25 @@ function setTime() {
     // When Timer Runs Out
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      timeUpMessage();
+      showResults();
     }
 
   }, 1000);
 }
 
-// When Given Time is Done
-function timeUpMessage() {
-  mainEl.textContent = "Time is Up!"
+function buildQuiz(){
+    quizContainer.textContent = "Quiz Started" + alert(questions.length);
+
+    for (var i = 0; i < questions.length; i++) {
+        quizContainer.textContent += "d" + "<br>";
+    }
 }
+
+// Show Results
+function showResults(){
+}
+
+// display quiz right away
+buildQuiz();
 
 setTime();
